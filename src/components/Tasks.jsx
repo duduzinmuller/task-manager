e2d5cd5f6af3e07b80d4d4a27f7049eb2dfcc991
Eduardo <eduardomuller989@gmail.com>
2025-03-21 23:39:33 -0400

    feat: ensure toast is shown after task update

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -8,6 +8,7 @@ import MoonIcon from "../assets/icons/moon.svg?react"
 import { TasksSeparator } from "./TasksSeparator"
 import TASKS from "../constants/tasks"
 import TaskItem from "./TaskItem"
+import { toast } from "sonner"
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS)
@@ -19,6 +20,7 @@ const Tasks = () => {
   const handleTaskDeleteClick = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId)
     setTasks(newTasks)
+    toast.success("Tarefa deletada com succeso!")
   }
 
   const handleTaskCheckboxClick = (taskId) => {
@@ -28,14 +30,17 @@ const Tasks = () => {
       }
 
       if (task.status === "not_started") {
+        toast.success("Tarefa iniciada com sucesso!")
         return { ...task, status: "in_progress" }
       }
 
       if (task.status === "in_progress") {
+        toast.success("Tarefa concluída com sucesso!")
         return { ...task, status: "done" }
       }
 
       if (task.status === "done") {
+        toast.success("Tarefa reiniciada com sucesso!")
         return { ...task, status: "not_started" }
       }
 

a07bc401f346192df69e46bd614e3798f5c30286
Eduardo <eduardomuller989@gmail.com>
2025-03-23 16:07:02 -0400

    refactor: add design system

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,7 +8,7 @@ const Input = forwardRef(({ label, errorMessage, ...rest }, ref) => {
     <div className="flex flex-col space-y-1 text-left">
       <InputLabel htmlFor={rest.id}>{label}</InputLabel>
       <input
-        className={`rounded-lg border border-solid px-4 py-3 outline-[#00AD85] placeholder:text-sm ${
+        className={`rounded-lg border border-solid border-brand-border px-4 py-3 text-brand-dark-blue outline-brand-primary placeholder:text-sm ${
           errorMessage ? "border-red-500 placeholder:text-red-500" : ""
         }`}
         ref={ref}
--- src/components/InputLabel.jsx
+++ src/components/InputLabel.jsx
@@ -1,6 +1,9 @@
 const InputLabel = (props) => {
   return (
-    <label className="text-sm font-semibold text-[#35383E]" {...props}>
+    <label
+      className="text-brand-dark-blue-dark-blue text-sm font-semibold"
+      {...props}
+    >
       {props.children}
     </label>
   )

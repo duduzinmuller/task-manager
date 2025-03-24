79984fa53d181bfe5e4e1fa3e46a913b4175b9d3
Eduardo <eduardomuller989@gmail.com>
2025-03-23 20:42:31 -0400

    chore: add prep-types to SidebarButton component

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 import { tv } from "tailwind-variants"
 
 const SidebarButton = ({ children, color }) => {
@@ -5,8 +6,8 @@ const SidebarButton = ({ children, color }) => {
     base: "flex items-center gap-2 rounded-lg px-6 py-3",
     variants: {
       color: {
-        unselected: "text-brand-dark-blue",
         selected: "bg-brand-primary bg-opacity-15 text-brand-primary",
+        unselected: "text-brand-dark-blue",
       },
     },
   })
@@ -16,6 +17,11 @@ const SidebarButton = ({ children, color }) => {
       {children}
     </a>
   )
+}
+
+SidebarButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(["selected", "unselected"]),
 }
 
 export default SidebarButton

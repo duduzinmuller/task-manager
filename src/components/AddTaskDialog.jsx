77e0ccdf81e046b8604391ef822b8f688b1eb881
Eduardo <eduardomuller989@gmail.com>
2025-03-24 01:14:09 -0400

    feat: ensure inputs are disabled on loading on AddTaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -110,9 +110,14 @@ const AddTaskDialog = ({
                   placeholder="Titulo da tarefa"
                   errorMessage={titleError?.message}
                   ref={titleRef}
+                  disabled={isLoading}
                 />
 
-                <TimeSelect ref={timeRef} errorMessage={timeError?.message} />
+                <TimeSelect
+                  errorMessage={timeError?.message}
+                  ref={timeRef}
+                  disabled={isLoading}
+                />
 
                 <Input
                   id="description"
@@ -120,6 +125,7 @@ const AddTaskDialog = ({
                   placeholder="Descreva a tarefa"
                   errorMessage={descriptionError?.message}
                   ref={descriptionRef}
+                  disabled={isLoading}
                 />
 
                 <div className="flex gap-3">

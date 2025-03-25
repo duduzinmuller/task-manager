918037d48fdd5fab614f7bd2ecdac5cc5140c95e
Eduardo <eduardomuller989@gmail.com>
2025-03-24 22:08:03 -0400

    feat: ensure task can be deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { Toaster } from "sonner"
-
 import Sidebar from "./components/Sidebar"
 import Tasks from "./components/Tasks"
 
 function App() {
   return (
     <div className="flex">
-      <Toaster
-        toastOptions={{
-          style: {
-            color: "#35383E",
-          },
-          duration: 3000,
-        }}
-      />
       <Sidebar />
       <Tasks />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Toaster } from "sonner"
 
 import App from "./App.jsx"
 import TaskDetailsPage from "./pages/task-details.jsx"
@@ -20,6 +21,14 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Toaster
+      toastOptions={{
+        style: {
+          color: "#35383E",
+        },
+        duration: 3000,
+      }}
+    />
     <RouterProvider router={router} />
   </StrictMode>
 )

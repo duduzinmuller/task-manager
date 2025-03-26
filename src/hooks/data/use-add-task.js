9961644add79fcc01afd5a79ba3196e21f7265da
Eduardo <eduardomuller989@gmail.com>
2025-03-25 22:40:18 -0400

    refactor: add axios

--- src/hooks/data/use-add-task.js
+++ src/hooks/data/use-add-task.js
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios from "axios"
 import { toast } from "sonner"
 
 export const useAddTask = () => {
@@ -6,14 +7,10 @@ export const useAddTask = () => {
   return useMutation({
     mutationKey: "addTask",
     mutationFn: async (task) => {
-      const response = await fetch("http://localhost:3000/tasks", {
-        method: "POST",
-        body: JSON.stringify(task),
-      })
-      if (!response.ok) {
-        throw new Error()
-      }
-      const createdTask = await response.json()
+      const { data: createdTask } = await axios.post(
+        "http://localhost:3000/tasks",
+        task
+      )
       return createdTask
     },
     onSuccess: (createdTask) => {
--- src/hooks/data/use-get-by-id-task.js
+++ src/hooks/data/use-get-by-id-task.js
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
 export const useGetByIdTask = ({ taskId, onSuccess }) => {
   return useQuery({
     queryKey: ["task", taskId],
     queryFn: async () => {
-      const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
-        method: "GET",
-      })
-      const task = await response.json()
+      const { data: task } = await await axios.get(
+        `http://localhost:3000/tasks/${taskId}`
+      )
       onSuccess(task)
       return task
     },
